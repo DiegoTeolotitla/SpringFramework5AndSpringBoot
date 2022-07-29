41cd94c4b243dd4835a67774ed81219bfc424610
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-29 01:23:52 -0500

    Añadiendo la clase servicio

--- spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/controllers/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/controllers/IndexController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bolsaideas.springboot.di.app.models.service.Miservicio;
+
 @Controller
 public class IndexController {
 	
+	private Miservicio servicio = new Miservicio();
+	
 	@GetMapping({"/index","/",""})
 	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
 }
---
+++ spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/models/service/Miservicio.java
@@ -0,0 +1,9 @@
+package com.bolsaideas.springboot.di.app.models.service;
+
+
+public class Miservicio {
+
+	public String operacion() {
+		return "ejecutando algun proceso importante...";
+	}
+}
