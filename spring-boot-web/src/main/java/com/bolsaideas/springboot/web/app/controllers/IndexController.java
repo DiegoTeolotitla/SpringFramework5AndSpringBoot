b8662d210bb9cd65b075923f33ad146b1623374d
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-29 00:12:45 -0500

    Agregando otros archivos properties personalizados para textos

--- spring-boot-web/src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsaideas.springboot.web.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,16 @@ import com.bolsaideas.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+
 	@GetMapping({ "/index", "/", "/home" })
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola SPRING FRAMEWORK");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
 
@@ -28,18 +36,18 @@ public class IndexController {
 		usuario.setNombre("Diego");
 		usuario.setEmail("diego@hotmail.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
-	
+
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de Usuarios");
+		model.addAttribute("titulo", textoListar);
 		return "listar";
 	}
-	
+
 	@ModelAttribute("usuarios")
-	public List<Usuario> poblarUsuario(){
+	public List<Usuario> poblarUsuario() {
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Diego", "Teolotitla", "diego@hotmail.com"));
 		usuarios.add(new Usuario("Armando", "Teolotitla", "armando@gmail.com"));
