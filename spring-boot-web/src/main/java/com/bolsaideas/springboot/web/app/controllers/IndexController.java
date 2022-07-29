f9536248d845ebbec1001329dc5e0fa8ebd857b1
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-28 19:28:48 -0500

    Directiva if de Thymeleaf

--- spring-boot-web/src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.bolsaideas.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +25,17 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setApellido("Teolotitla");
 		usuario.setNombre("Diego");
+		usuario.setEmail("diego@hotmail.com");
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
 		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo", "Listado de Usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
 	}
 }
