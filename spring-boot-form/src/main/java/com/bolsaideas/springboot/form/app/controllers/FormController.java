b05dbe6c7f3aa4d4c49afd2b3c70d5748c03ad0a
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-31 18:43:08 -0500

    Añadiendo los mensajes de errores en la vista del formulario

--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
@@ -1,10 +1,15 @@
 package com.bolsaideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bolsaideas.springboot.form.app.models.domain.Usuario;
 
@@ -14,22 +19,24 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", new Usuario());
 		return "form";
 	}
-	
+
 	@PostMapping("/form")
-	public String procesarFormulario(Model model, 
-			@RequestParam(name = "username") String username,
-			@RequestParam(name = "password") String password,
-			@RequestParam(name = "email") String email) {
-		Usuario usuario = new Usuario();
-		usuario.setUsername(username);
-		usuario.setPassword(password);
-		usuario.setEmail(email);
+	public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errores.put(err.getField(),
+						"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
 		model.addAttribute("titulo", "Resultado  form");
 		model.addAttribute("usuario", usuario);
-		
-		
+
 		return "resultado";
 	}
 }
