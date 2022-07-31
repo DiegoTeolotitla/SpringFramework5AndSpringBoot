d4ec5d4011fb21c0cf76c3c5f67b4defc9e3ab01
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-31 16:03:47 -0500

    Ciclo de vide del componente @PostConstruct @PreDestroy

--- spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.bolsaideas.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +20,17 @@ public class Factura {
 	@Autowired
 	@Qualifier("itemsFacturaOficina")
 	private List<ItemFactura> items;
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Armando"));
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
 
 	public String getDescripcion() {
 		return descripcion;
