d500426381eae119400af21c9acf89fbda532f9a
Diego.Teolotitla <diego.teolotitla@outlook.com>
2022-07-31 19:43:43 -0500

    Creando la vista listar.html

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES (1, 'Diego','Teolotitla', 'diego@hotmail.com', '2022-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES (2, 'Diego Armando','Teolotitla', 'diegoA@hotmail.com', '2022-08-29');
